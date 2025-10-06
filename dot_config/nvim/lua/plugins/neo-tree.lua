0a62228c545d1bbfe47f6347118dd0a7e41ace0a
github-oj <oj-software-projects@gmail.com>
2025-10-06 21:51:03 +0200

    changed: fixed .env and other files are visible with H

---
+++ dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {},
+        hide_by_pattern = {},
+        never_show = {},
+        never_show_by_pattern = {},
+      },
+    },
+  },
+}
