4b7a3fb24013aa5558c028fb2aa3cb1137e49bce
github-oj <oj-software-projects@gmail.com>
2025-10-29 01:16:43 +0100

    changed: added lua.tmpl config so that lsp works

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+
+-- Treat template files ending in .lua.tmpl as Lua so LSP and completion work.
+vim.filetype.add({
+  pattern = {
+    [".*%.lua%.tmpl"] = "lua",
+  },
+})
